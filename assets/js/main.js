e8b5d73eadafb050129f8de4e172612e5369f99d
niccolosantelli <niccolo_santelli@live.it>
2021-01-13 17:30:39 +0100

    milestone2
    fatto anche serietv ma è da sistemare

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,8 @@ methods:{
       language: "it-IT"
  }
 })
+
+
   .then(response => {
     let ritorno = response.data.results
     this.filmDocuments = ritorno
@@ -24,12 +26,32 @@ methods:{
      this.filmDocuments.forEach(item => {
        const vote = Math.ceil(item.vote_average / 2)
        console.log(vote);
-       return item.stellePiene = vote;      
+       return item.stellePiene = vote;
      });
-
-
-
     })
+
+
+
+    axios.get("https://api.themoviedb.org/3/search/tv", {
+      params: {
+        api_key:"a1585e4975fa966c97fa497f209af976",
+        query:this.searchTerm,
+        language: "it-IT"
    }
+  })
+  .then(response => {
+    let ritorno = response.data.results
+    this.filmDocuments = ritorno
+    console.log(ritorno);
+
+     this.filmDocuments.forEach(item => {
+       const vote = Math.ceil(item.vote_average / 2)
+       console.log(vote);
+       return item.stellePiene = vote;
+     });
+    })
+
+
+    }
   }
 })
