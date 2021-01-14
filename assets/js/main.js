6a3824960191443ee5fd64d7ea8b36c89a6307e4
niccolosantelli <niccolo_santelli@live.it>
2021-01-14 11:18:11 +0100

    milestone 2 completa

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ let app = new Vue({
   data: {
   searchTerm: "",
   filmDocuments: [],
+  tvSeriesDocuments:[],
 
 
 },
@@ -16,40 +17,38 @@ methods:{
       language: "it-IT"
  }
 })
+      .then(response => {
+        let ritorno = response.data.results
+        this.filmDocuments = ritorno
+        console.log(ritorno);
+
+         this.filmDocuments.forEach(item => {
+           const vote = Math.ceil(item.vote_average / 2)
+           console.log(vote);
+           return item.stellePiene = vote;
+         });
+        })
 
 
-  .then(response => {
-    let ritorno = response.data.results
-    this.filmDocuments = ritorno
-    console.log(ritorno);
 
-     this.filmDocuments.forEach(item => {
-       const vote = Math.ceil(item.vote_average / 2)
-       console.log(vote);
-       return item.stellePiene = vote;
-     });
+      axios.get("https://api.themoviedb.org/3/search/tv", {
+        params: {
+          api_key:"a1585e4975fa966c97fa497f209af976",
+          query:this.searchTerm,
+          language: "it-IT"
+     }
     })
+      .then(response => {
+        let ritorno = response.data.results
+        this.tvSeriesDocuments = ritorno
+        console.log(ritorno);
 
-
-
-    axios.get("https://api.themoviedb.org/3/search/tv", {
-      params: {
-        api_key:"a1585e4975fa966c97fa497f209af976",
-        query:this.searchTerm,
-        language: "it-IT"
-   }
-  })
-  .then(response => {
-    let ritorno = response.data.results
-    this.filmDocuments = ritorno
-    console.log(ritorno);
-
-     this.filmDocuments.forEach(item => {
-       const vote = Math.ceil(item.vote_average / 2)
-       console.log(vote);
-       return item.stellePiene = vote;
-     });
-    })
+          this.tvSeriesDocuments.forEach(item => {
+            const vote = Math.ceil(item.vote_average / 2)
+            console.log(vote);
+            return item.stellePiene = vote;
+          });
+        })
 
 
     }
